f2d42558576f6c68082e19943e326c8cc8e2fd46
MightyPhoenix <agnibesh00mukherjee@gmail.com>
2021-04-10 00:50:07 +0530

    add post modal

--- src/components/User/Feeds/Feed.js
+++ src/components/User/Feeds/Feed.js
@@ -1,34 +1,48 @@
 import React, { useState, useEffect } from "react";
 import fire from "../../../config/Fire";
 import FeedItem from "./FeedItem";
+import { Modal } from "../../Util/Modal";
 
 const Feed = () => {
+  const [feeds, setFeeds] = useState([]);
+  const [modal, setModal] = useState(false);
 
-    const [feeds, setFeeds] = useState([]);
+  useEffect(() => {
+    getFeeds();
+  }, []);
 
-    useEffect(() => {
-        
-        getFeeds();
-    }, [])
+  const getFeeds = async () => {
+    const feedDocs = await fire
+      .firestore()
+      .collection("feed")
+      .doc("feeds")
+      .get();
+    const feedData = await feedDocs.data();
+    setFeeds(feedData?.feeds);
+  };
+  return (
+    <>
+      <div className="flex flex-row px-auto py-auto">
+        {feeds.map((feed, key) => {
+          return <FeedItem feed={feed} key={key} />;
+        })}
+      </div>
 
-    const getFeeds = async () => {
-        const feedDocs = await fire.firestore().collection("feed").doc("feeds").get();
-        const feedData = await feedDocs.data();
-        setFeeds(feedData?.feeds);
-    }
-    return (
-        <div className="flex flex-row px-auto py-auto">
-            {
-                feeds.map((feed,key) => {
-                    return(
-                        <FeedItem feed={feed} key={key} />
-                    )
-                }
-                    
-                )
-            }
-        </div>
-    )
-}
+      <button
+        onClick={() => setModal(true)}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full max-w-xs ml-auto relative right-0 bottom-0"
+      >
+        Create New Post
+      </button>
+      <Modal
+        isOpen={modal}
+        close={() => setModal(false)}
+        title={`Create New Post`}
+          >
+              <h1>What would you like to post today?</h1>
+      </Modal>
+    </>
+  );
+};
 
 export default Feed;
